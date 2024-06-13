75c0f9f9e0192cdd44f1302de1644a4eca0e722a
Lucia Liu <112028165+lliu285@users.noreply.github.com>
2024-06-12 19:11:04 -0700

    work on hash table

--- src/lab05/HashTable.java
+++ src/lab05/HashTable.java
@@ -1,5 +1,52 @@
 package lab05;
 
-public class HashTable {
-
+public class HashTable 
+{
+	private int size;
+	private Dollar[] items;
+	private int count;
+	private int collisions;
+	
+	public HashTable(int size)
+	{
+		this.size = size;
+		items = new Dollar[size];
+		count = 0;
+		collisions = 0;
+	}
+	
+	public int getSize()
+	{
+		return size;
+	}
+	
+	public int getCount()
+	{
+		return count;
+	}
+	
+	public int getCollisions()
+	{
+		return collisions;
+	}
+	
+	public Dollar[] getItems()
+	{
+		return items;
+	}
+	
+	public void insert()
+	{
+		
+	}
+	
+	public void remove()
+	{
+		
+	}
+	
+	public void search()
+	{
+		
+	}
 }
